c5bbbb749a0f65b60bead0b5df5a17f5e0a5b0b2
José Luis Ávila Guzmán <joselavila13@hotmail.com>
2020-01-21 16:40:11 -0500

    Find a permutation in istream

--- String permutations.cpp
+++ String permutations.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 void exchange(string, int);
 string swap(const string&, int, int);
-
+bool problemFunction(string);
 
 int main()
 {
-    string example = "ABCDEFGHIJKLMNOPQ";
-    exchange(example, 0);
-    return 0;
+    exchange("tac", 0);
 }
 
 
@@ -18,16 +17,14 @@ int main()
     Prints all the permutations of a given string.
     Note: All the characters of the string must be different, otherwise
     there will be duplicate results
-
     @param characters String to be exchanged
     @param startPosition Start position of the string to be operated
-
 */
 void exchange(string characters, int startPosition)
 {
     int longitudcharacters = characters.length();
-    if (startPosition == longitudcharacters - 1) // s is the last character
-        cout << characters << endl;
+    if (startPosition == longitudcharacters - 1 and problemFunction(characters)) // s is the last character
+        cout << characters << "Is a possible solution" << endl;
     else
         for (int t = startPosition; t <= (longitudcharacters - 1); t++)
         {
@@ -38,7 +35,6 @@ void exchange(string characters, int startPosition)
 
 /*
     Swap the values of two given positions of a string
-
     @param cad The string where the characters will be swaped
     @param pos_a , pos_b String positions to be swaped
 */
@@ -49,4 +45,22 @@ string swap(const string& cad, int pos_a, int pos_b)
     resutlString[pos_a] = resutlString[pos_b];
     resutlString[pos_b] = temp;
     return resutlString;
+}
+
+bool problemFunction(string a)
+{
+    ifstream animalTable("nature.dat", ios::in);
+    
+    //If file couldn't be opened
+    if (!animalTable)
+    {
+        cerr << "Could not open .dat file";
+        exit(1);
+    } 
+
+    string currentAnimal("");
+    while (animalTable >> currentAnimal)
+        if (a == currentAnimal)
+            return true;   
+    return false;
 }
